af837b6d7198bbc2473af58c86fc157207a514d4
L-Train <train19@gmail.com>
2019-07-14 01:48:53 -0400

    Updates

---
+++ precache-manifest.111443ae1b40ff48671f242251d1289f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7c68d6821d59ecc370b09ce8266f67",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "6e5bebe9a8019ffaa57f",
+    "url": "/ClickyGame/static/css/main.4725ece7.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ClickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6e5bebe9a8019ffaa57f",
+    "url": "/ClickyGame/static/js/main.82a0146e.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
